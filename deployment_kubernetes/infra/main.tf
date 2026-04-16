47b7d47c17a20c4eb960f683d66ef8fe26b8cc98
Jakub Dadello <kejdi16try@gmail.com>
2026-04-16 14:06:29 +0200

    create main.tf

---
+++ deployment_kubernetes/infra/main.tf
@@ -0,0 +1,242 @@
+# ============================================================
+# Required providers (declaration)
+# ============================================================
+terraform {
+    required_providers {
+        kubernetes = {
+            source  = "hashicorp/kubernetes"
+            version = "~> 2.30"
+        }
+    }
+}
+
+# ============================================================
+# providers (configuration)
+# ============================================================
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube-context"
+}
+
+# ============================================================
+# 1. MODEL STORAGE (MinIO)
+# ============================================================
+
+# DEPLOYMENT
+resource "kubernetes_deployment" "minio" {
+  metadata {
+    name = "model-storage"
+
+    labels = {
+      app = "model-storage"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+          app = "model-storage"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "model-storage"
+        }
+      }
+
+      spec {
+        container {
+          name  = "minio"
+          image = "minio/minio:latest"
+
+          args = ["server", "/data"]
+
+          port {
+            container_port = 9000
+          }
+
+          volume_mount {
+            name       = "data"
+            mount_path = "/data"
+          }
+
+          resources {
+            requests = {
+              cpu = "100m"
+              memory = "256Mi"
+            }
+
+            limits = {
+              cpu = "300m"
+              memory = "512Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+# SERVICE 
+resource "kubernetes_service" "minio" {
+  metadata {
+    name = "model-storage"
+  }
+
+  spec {
+    selector = {
+      app = "model-storage"
+    }
+
+    port {
+      port        = 9000
+      target_port = 9000
+    }
+
+    type = "NodePort" #Load Balancer following an on-premises deployment 
+
+  }
+}
+
+# ============================================================
+# 2. INFERENCE API 
+# ============================================================
+
+# DEPLOYMENT
+resource "kubernetes_deployment" "app" {
+  metadata {
+    name = "ml-inference"
+
+    labels = {
+      app = "ml-inference"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "ml-inference"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "ml-inference"
+        }
+      }
+
+      spec {
+        container {
+          name  = "api"
+          image = "nginx:latest"
+
+          port {
+            container_port = 80
+          }
+
+          readiness_probe {
+            http_get {
+              path = "/"
+              port = 80
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 80
+            }
+          }
+
+          resources {
+            requests = {
+              cpu = "300m"
+              memory = "512Mi"
+            }
+
+            limits = {
+              cpu = "1000m"
+              memory = "1Gi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+# SERVICE 
+resource "kubernetes_service" "app" {
+  metadata {
+    name = "ml-inference"
+  }
+
+  spec {
+    selector = {
+       app = "ml-inference"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "ClusterIP"
+  }
+}
+
+# ============================================================
+# 3. CONFIGMAP (non-sensitive config)
+# ============================================================
+resource "kubernetes_config_map" "ml_config" {
+  metadata {
+    name = "ml-config"
+  }
+
+  data = {
+    MODEL_STORAGE_URL = "http://model-storage:9000"
+    INFERENCE_MODE = "dev"
+    LOG_LEVEL = "debug"
+  }
+}
+
+# ============================================================
+# 4. SECRET (sensitive data)
+# ============================================================
+resource "kubernetes_secret" "ml_secret" {
+  metadata {
+    name = "ml_secret"
+  }
+
+  data = {
+    MINIO_ACCESS_KEY = base64encode("admin")
+    MINIO_SECRET_KEY = base64encode("supersecret")
+  }
+  
+}
+
+# ============================================================
+# 4. PERSISTENT VOLUME CLAIM 
+# ============================================================
+resource "kubernetes_persistent_volume_claim" "minio" { #StorageClass (dynamic provisioning)
+  metadata {
+    name = "pvc"
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+  }
+}
